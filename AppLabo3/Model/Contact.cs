438cd285c101914736d4f86cc41225771a6ba6e3
Matthieu Meunier <matth.meun@hotmail.com>
2016-10-03 09:34:04 +0200

    Done

---
+++ AppLabo3/Model/Contact.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Contact
+    {
+        private string _firstname;
+
+        public string Firstname
+        {
+            get { return _firstname; }
+            set { _firstname = value; }
+        }
+
+        private string _lastname;
+
+        public string Lastname
+        {
+            get { return _lastname; }
+            set { _lastname = value; }
+        }
+
+        private string _email;
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        public Contact(string firstname, string lastname, string email)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+            Email = email;
+        }
+
+    }
+}
